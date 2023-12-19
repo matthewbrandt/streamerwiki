a6bbd3649a87168af2e4e758974f0b7819fa52fb
mattytwoshoes <matthewbrandt@gmail.com>
2023-12-19 15:52:19 +0100

    readme and contribution update

--- feature-docs/docusaurus.config.js
+++ feature-docs/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/matthewbrandt/twitch-features-docs',
-            label: 'GitHub',
+            label: 'Contribute',
             position: 'right',
           },
         ],
